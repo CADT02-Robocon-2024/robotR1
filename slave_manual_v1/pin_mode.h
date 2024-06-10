fcb274655da241f4e75c78cc9cb405a1b6d270b8
phana25 <sophatsophana25@gmail.com>
2024-06-10 14:54:20 +0700

    change code

--- slave_manual_v1/pin_mode.h
+++ slave_manual_v1/pin_mode.h
@@ -2,13 +2,21 @@
 #define mid_value 1002
 #define max_value 1722
 
-#define solen_G_up_R 34 //Solenoid Gripper up on the right
-#define solen_G_do_R 35 //Solenoid Gripper down on the right
+#define solen_G_up_R 35 //Solenoid Gripper up on the right
+#define solen_G_do_R 34 //Solenoid Gripper down on the right
 #define solen_G_L 38 //Solenoid Gripper up on the left
-#define solen_S_up_R 30 //Solenoid Slider up on the right
-#define solen_S_do_R 31 //Solenoid Slider down on the right
+#define solen_S_up_R 31 //Solenoid Slider up on the right
+#define solen_S_do_R 30 //Solenoid Slider down on the right
 
-bool pickup_R;
+
+bool pickup_R = false;
+bool motor_stop = false;
+bool release = false;
+bool ch5 = false;
+bool ch9 = false;
+
+unsigned long previousMillis = 0;
+const unsigned long interval = 2000; 
 
 void solenoid_init(){
   pinMode(solen_G_up_R, OUTPUT);
